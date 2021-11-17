85eaf7ceb58b4d91cb80d4a093a55a20b6ba8673
Deepak Kumar <deepakstark23@gmail.com>
2021-11-17 23:50:02 +0530

    scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,17 @@ void main()
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar:AppBar(
           title: Text("first app"),
+          backgroundColor: Colors.red,
+        ),
+        body:Center(
+          child: Image(
+            image:NetworkImage('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+            ),
         ),
         ),
-    ),
+        ),
   );
 }
